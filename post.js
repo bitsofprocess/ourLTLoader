7a5f3a591372ede3dd37abc1a65a75ce31413a7d
Dinelle Zipser <dinellezipser@gmail.com>
2022-09-18 06:20:27 -0700

    batchWrite added to post fxn

--- post.js
+++ post.js
@@ -50,8 +50,36 @@ const postOurtLT = async (file, tableName, dynamodb, title, owner) => {
 
     const wrappedQuestionSet = await wrapQuestionSet(newSetId, owner, title, structuredQuestions);
 
-    const updatedTable = await addToExistingTable(wrappedQuestionSet, dynamoTable);
-  
+    const updatedTable = await addToExistingTable(wrappedQuestionSet, dynamoTable)
+    
+    // https://stackoverflow.com/questions/60695821/dynamodb-exception-supplied-attributevalue-has-more-than-one-datatypes-set
+
+    // console.log(JSON.stringify(params))
+
+    let params = {
+      RequestItems: {
+        'ourLT-prod': [
+          {
+            PutRequest: {
+             Item: {
+              team_id: 'FIEO',
+              question_sets: 'test'
+             }
+            }
+          }
+        ]
+      }
+    }
+
+
+    try {
+      res = await dynamodb.batchWrite(params).promise()
+      let data = res;
+      console.log('Processed: ', JSON.stringify(updatedTable, null, 3))
+    } catch(err) {
+      console.log(err)
+    }
+
     // const success = await addToDynamo(team_id, wrappedArray);
   }
 };
