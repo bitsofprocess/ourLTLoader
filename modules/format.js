7248d06619a708b30a6279ad7653ec209e5d4856
Dinelle Zipser <dinellezipser@gmail.com>
2022-10-26 21:36:19 -0400

    fixed getNewSetId to work for specified team_id

--- modules/format.js
+++ modules/format.js
@@ -1,77 +1,82 @@
-const csv = require("csvtojson");
+const csv = require('csvtojson');
 
 module.exports.csvToJson = async (csvFilePath) => {
-  try {
-    const data = await csv().fromFile(csvFilePath);
+	try {
+		const data = await csv().fromFile(csvFilePath);
 
-    // log the JSON array
-    return data;
-  } catch (err) {
-    console.log(err);
-  }
+		// log the JSON array
+		return data;
+	} catch (err) {
+		console.log(err);
+	}
 };
 
 module.exports.assignIndexes = async (questionsArray) => {
-  function generateRandomInteger(max) {
-    return Math.floor(Math.random() * max);
-  }
+	function generateRandomInteger(max) {
+		return Math.floor(Math.random() * max);
+	}
 
-  let newJson = questionsArray.map((question) => {
-    const correct_answer = generateRandomInteger(4);
-    const incorrectAnswerArray = [
-      question.wrong_answer_1,
-      question.wrong_answer_2,
-      question.wrong_answer_3,
-    ];
+	let newJson = questionsArray.map((question) => {
+		const correct_answer = generateRandomInteger(4);
+		const incorrectAnswerArray = [
+			question.wrong_answer_1,
+			question.wrong_answer_2,
+			question.wrong_answer_3,
+		];
 
-    let shuffledAnswers = incorrectAnswerArray.sort(
-      (a, b) => 0.5 - Math.random()
-    );
-    shuffledAnswers.splice(correct_answer, 0, question.correct_answer);
+		let shuffledAnswers = incorrectAnswerArray.sort(
+			(a, b) => 0.5 - Math.random()
+		);
+		shuffledAnswers.splice(correct_answer, 0, question.correct_answer);
 
-    const json = {
-      question: question.question,
-      correct_answer: correct_answer,
-      answers: shuffledAnswers,
-    };
+		const json = {
+			question: question.question,
+			correct_answer: correct_answer,
+			answers: shuffledAnswers,
+		};
 
-    return json;
-  });
+		return json;
+	});
 
-  return newJson;
+	return newJson;
 };
 
-module.exports.getNewSetId = async (dynamoTable) => {
-  const questionSets = dynamoTable[0].question_sets;
+module.exports.getNewSetId = async (dynamoTable, team_id) => {
+	let newSetId;
 
-  let existingSetIds = [];
+	dynamoTable.forEach((element) => {
+		if (element.team_id === team_id) {
+			let existingSetIds = [];
+			const questionSets = element.question_sets;
 
-  questionSets.forEach((set) => existingSetIds.push(set.set_id));
+			questionSets.forEach((set) => existingSetIds.push(set.set_id));
 
-  const newSetId = Math.max(...existingSetIds) + 1;
+			newSetId = Math.max(...existingSetIds) + 1;
+		}
+	});
 
-  return newSetId;
+	return newSetId;
 };
 
 module.exports.wrapQuestionSet = async (
-  newSetId,
-  owner,
-  title,
-  structuredQuestions
+	newSetId,
+	owner,
+	title,
+	structuredQuestions
 ) => {
-  let obj = {
-    set_id: newSetId,
-    owner: owner,
-    title: title,
-    questions: structuredQuestions,
-  };
+	let obj = {
+		set_id: newSetId,
+		owner: owner,
+		title: title,
+		questions: structuredQuestions,
+	};
 
-  return obj;
+	return obj;
 };
 
 module.exports.addToExistingTable = async (wrappedQuestionSet, dynamoTable) => {
-  const newTable = dynamoTable[0];
-  newTable.question_sets.push(wrappedQuestionSet);
+	const newTable = dynamoTable[0];
+	newTable.question_sets.push(wrappedQuestionSet);
 
-  return newTable.question_sets;
+	return newTable.question_sets;
 };
