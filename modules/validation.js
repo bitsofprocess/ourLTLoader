3ee8aa8394dcffe20c2105088c0fcf0fde481f65
Dinelle Zipser <dinellezipser@gmail.com>
2022-11-03 11:05:41 -0700

    compare titles passes if team_id not in table

--- modules/validation.js
+++ modules/validation.js
@@ -1,172 +1,175 @@
 module.exports.checkAllValues = async (questionsArray) => {
-  let valueCheck = [];
-  let passedValueCheck;
+	let valueCheck = [];
+	let passedValueCheck;
 
-  questionsArray.forEach((element) => {
-    const valuesArray = Object.values(element);
+	questionsArray.forEach((element) => {
+		const valuesArray = Object.values(element);
 
-    if (valuesArray.includes("")) {
-      valueCheck.push(false);
-    } else {
-      valueCheck.push(true);
-    }
-  });
+		if (valuesArray.includes('')) {
+			valueCheck.push(false);
+		} else {
+			valueCheck.push(true);
+		}
+	});
 
-  if (valueCheck.includes(false)) {
-    passedValueCheck = false;
-  } else {
-    passedValueCheck = true;
-  }
+	if (valueCheck.includes(false)) {
+		passedValueCheck = false;
+	} else {
+		passedValueCheck = true;
+	}
 
-  return passedValueCheck;
+	return passedValueCheck;
 };
 
 module.exports.maxLengthCheck = async (questionsArray) => {
-  const lengthCheck = [];
-  let passesLengthCheck;
+	const lengthCheck = [];
+	let passesLengthCheck;
 
-  questionsArray.forEach((element) => {
-    for (key in element) {
-      if (element[key].length < 200) {
-        lengthCheck.push(true);
-      } else {
-        lengthCheck.push(false);
-      }
-    }
-  });
+	questionsArray.forEach((element) => {
+		for (key in element) {
+			if (element[key].length < 200) {
+				lengthCheck.push(true);
+			} else {
+				lengthCheck.push(false);
+			}
+		}
+	});
 
-  if (lengthCheck.includes(false)) {
-    passesLengthCheck = false;
-  } else {
-    passesLengthCheck = true;
-  }
-  return passesLengthCheck;
+	if (lengthCheck.includes(false)) {
+		passesLengthCheck = false;
+	} else {
+		passesLengthCheck = true;
+	}
+	return passesLengthCheck;
 };
 
 module.exports.questionFreeOfSpecialChar = async (str) => {
-  const specialChars = /[^`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/~]/;
-  return specialChars.test(str);
+	const specialChars = /[^`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/~]/;
+	return specialChars.test(str);
 };
 
 module.exports.answerFreeOfSpecialChar = async (str) => {
-  const specialChars = /[^`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
-  return specialChars.test(str);
+	const specialChars = /[^`!@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?~]/;
+	return specialChars.test(str);
 };
 
 module.exports.checkForSpecialChar = async (questionsArray) => {
-  const requiredKeys = [
-    "question",
-    "correct_answer",
-    "wrong_answer_1",
-    "wrong_answer_2",
-    "wrong_answer_3",
-  ];
+	const requiredKeys = [
+		'question',
+		'correct_answer',
+		'wrong_answer_1',
+		'wrong_answer_2',
+		'wrong_answer_3',
+	];
 
-  let validationArray = [];
-  let passedValidation;
+	let validationArray = [];
+	let passedValidation;
 
-  questionsArray.forEach((element) => {
-    for (const key of requiredKeys) {
-      if (key === "question") {
-        let result = exports.questionFreeOfSpecialChar(element[key]);
-        validationArray.push(result);
-      } else if (
-        key === "wrong_answer_1" ||
-        "wrong_answer_2" ||
-        "wrong_answer_3"
-      ) {
-        let result = exports.answerFreeOfSpecialChar(element[key]);
-        validationArray.push(result);
-      }
-    }
-  });
+	questionsArray.forEach((element) => {
+		for (const key of requiredKeys) {
+			if (key === 'question') {
+				let result = exports.questionFreeOfSpecialChar(element[key]);
+				validationArray.push(result);
+			} else if (
+				key === 'wrong_answer_1' ||
+				'wrong_answer_2' ||
+				'wrong_answer_3'
+			) {
+				let result = exports.answerFreeOfSpecialChar(element[key]);
+				validationArray.push(result);
+			}
+		}
+	});
 
-  if (validationArray.includes(false)) {
-    passedValidation = false;
-  } else {
-    passedValidation = true;
-  }
-  return passedValidation;
+	if (validationArray.includes(false)) {
+		passedValidation = false;
+	} else {
+		passedValidation = true;
+	}
+	return passedValidation;
 };
 
 module.exports.compareTitles = async (dynamoTable, title, team_id) => {
-  let titleArray = [];
-  let titleCheckPassed;
+	let titleCheckPassed;
+	let teamIdArray = dynamoTable.map((teamObj) => teamObj.team_id);
 
-  dynamoTable.forEach(object => {
-    if (object.team_id === team_id) {
-      const questionSetArray = object.question_sets;
-      questionSetArray.forEach(set => titleArray.push(set.title));
-    }
-  })
+	if (teamIdArray.includes(team_id)) {
+		dynamoTable.forEach((teamObj) => {
+			if (teamObj.team_id === team_id) {
+				const questionSetArray = teamObj.question_sets;
+				let titleArray = questionSetArray.map(
+					(questionSet) => questionSet.title
+				);
+				if (!titleArray.includes(title)) {
+					titleCheckPassed = true;
+				} else titleCheckPassed = false;
+			}
+		});
+	} else {
+		return (titleCheckPassed = true);
+	}
 
-  if (titleArray.includes(title)) {
-    titleCheckPassed = false;
-  } else {
-    titleCheckPassed = true;
-  }
-
-  return titleCheckPassed;
+	return titleCheckPassed;
+  
 };
 
 module.exports.validateCriteria = async (validationCriteriaObject) => {
-  let csvPassesValidation;
-  const validationArray = Object.values(validationCriteriaObject);
+	let csvPassesValidation;
+	const validationArray = Object.values(validationCriteriaObject);
 
-  if (validationArray.includes(false)) {
-    csvPassesValidation = false;
-  } else {
-    csvPassesValidation = true;
-  }
+	if (validationArray.includes(false)) {
+		csvPassesValidation = false;
+	} else {
+		csvPassesValidation = true;
+	}
 
-  return csvPassesValidation;
+	return csvPassesValidation;
 };
 
 module.exports.getValidationDetails = async (
-  questionsArray,
-  title,
-  dynamoTable,
-  team_id
+	questionsArray,
+	title,
+	dynamoTable,
+	team_id
 ) => {
-  let passesCharCheck;
-  let allValuesPresent;
-  let passesLengthCheck;
-  let passesTitleCheck;
-  let validationCriteria = {
-    passesCharCheck,
-    allValuesPresent,
-    passesLengthCheck,
-    passesTitleCheck,
-  };
+	let passesCharCheck;
+	let allValuesPresent;
+	let passesLengthCheck;
+	let passesTitleCheck;
+	let validationCriteria = {
+		passesCharCheck,
+		allValuesPresent,
+		passesLengthCheck,
+		passesTitleCheck,
+	};
 
-  validationCriteria.passesCharCheck = await exports.checkForSpecialChar(
-    questionsArray
-  );
+	validationCriteria.passesCharCheck = await exports.checkForSpecialChar(
+		questionsArray
+	);
 
-  validationCriteria.allValuesPresent = await exports.checkAllValues(
-    questionsArray
-  );
+	validationCriteria.allValuesPresent = await exports.checkAllValues(
+		questionsArray
+	);
 
-  validationCriteria.passesLengthCheck = await exports.maxLengthCheck(
-    questionsArray
-  );
+	validationCriteria.passesLengthCheck = await exports.maxLengthCheck(
+		questionsArray
+	);
 
-  validationCriteria.passesTitleCheck = await exports.compareTitles(
-    dynamoTable,
-    title,
-    team_id
-  );
+	validationCriteria.passesTitleCheck = await exports.compareTitles(
+		dynamoTable,
+		title,
+		team_id
+	);
 
-  return validationCriteria;
+	return validationCriteria;
 };
 
 module.exports.checkTableForTeamId = async (dynamoTable, team_id) => {
-  let currentTeamIds = [];
-  dynamoTable.forEach(team => currentTeamIds.push(team.team_id));
-  if (!currentTeamIds.includes(team_id)) {
-    return false;
-  } else {
-    return true;
-  }
-}
-
+	let currentTeamIds = [];
+	dynamoTable.forEach((team) => currentTeamIds.push(team.team_id));
+	if (!currentTeamIds.includes(team_id)) {
+		return false;
+	} else {
+		return true;
+	}
+};
