e59a4f1468901e09b1448f1502f7a853c1bde741
Dinelle Zipser <dinellezipser@gmail.com>
2022-12-01 16:15:07 -0800

    removed unnecessary functions

--- modules/aws.js
+++ modules/aws.js
@@ -1,6 +1,6 @@
 // Load the AWS SDK for Node.js
 const AWS = require("aws-sdk");
-
+const dynamoTableName = process.env.dynamoTableName;
 module.exports.getDynamoTable = async (dynamodb) => {
   var params = {
     TableName: "ourLT-prod",
@@ -17,32 +17,7 @@ module.exports.getDynamoTable = async (dynamodb) => {
   }
 };
 
-module.exports.addNewTeamToDynamo = async (team_id, wrappedQuestionSet, dynamodb) => {
-  let params = {
-    RequestItems: {
-      "ourLT-prod": [
-        {
-          PutRequest: {
-            Item: {
-              team_id: team_id,
-              question_sets: [wrappedQuestionSet],
-            },
-          },
-        },
-      ],
-    },
-  };
-
-  try {
-    res = await dynamodb.batchWrite(params).promise();
-    let data = res;
-    console.log("Processed: ", `${JSON.stringify(params, null, 3)}`);
-  } catch (err) {
-    console.log(err);
-  }
-};
-
-module.exports.addQuestSetToExistingTeamInDynamo = async (team_id, updatedQuestionSetArray, dynamodb) => {
+module.exports.addQuestSetToDynamo = async (team_id, updatedQuestionSetArray, dynamodb) => {
   let params = {
     RequestItems: {
       "ourLT-prod": [
@@ -66,3 +41,30 @@ module.exports.addQuestSetToExistingTeamInDynamo = async (team_id, updatedQuesti
     console.log(err);
   }
 };
+
+module.exports.addNewTeamToDynamo = async (team_id, wrappedQuestionSet, dynamodb) => {
+
+  let PutRequest = {
+    PutRequest: {
+        Item: {
+          team_id: team_id,
+          question_sets: [wrappedQuestionSet],
+        }
+      }
+  }
+  let RequestItems = {};
+  RequestItems[dynamoTableName] = [];
+  RequestItems[dynamoTableName][0] = PutRequest;
+  let params = {};
+  params.RequestItems = RequestItems;
+
+
+  try {
+    res = await dynamodb.batchWrite(params).promise();
+    let data = res;
+    console.log("Processed: ", `${JSON.stringify(params, null, 3)}`);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
